a5021d2fa2a476cdecb4459490055fd4f1792ab3
NinoCuric <nino.solin@gmail.com>
2026-05-25 05:33:12 +0200

    card commit
    card Prefab and ScriptableObject

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+using UnityEngine;
+
+
+namespace Cards
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardame;
+        public CardType cardType;
+        public CardNumber baseNumber;
+        public int valueNumber;
+        public Sprite cardSprite;
+
+        public void InitializeNumber()
+        {
+            valueNumber = (int)baseNumber;
+        }
+
+        public enum CardType
+        {
+            Spades,
+            Hearts,
+            Diamonds,
+            Clubs
+        }
+
+        public enum CardNumber
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13
+        }
+
+    }
+}
